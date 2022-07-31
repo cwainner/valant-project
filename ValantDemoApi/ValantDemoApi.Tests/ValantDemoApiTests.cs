3320f8029cba541d5a9f6015fc1b2be7ffa5fde1
Chris Wainner <chris.jwainner@gmail.com>
2022-07-31 12:03:12 -0700

    Add endpoint to add a new maze.

--- ValantDemoApi/ValantDemoApi.Tests/ValantDemoApiTests.cs
+++ ValantDemoApi/ValantDemoApi.Tests/ValantDemoApiTests.cs
@@ -2,32 +2,52 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
 namespace ValantDemoApi.Tests
 {
-    [TestFixture]
-    public class ValantDemoApiTests
+  [TestFixture]
+  public class ValantDemoApiTests
+  {
+    private HttpClient client;
+
+    [OneTimeSetUp]
+    public void Setup()
     {
-      private HttpClient client;
-
-      [OneTimeSetUp]
-        public void Setup()
-        {
-          var factory = new APIWebApplicationFactory();
-          this.client = factory.CreateClient();
-        }
-
-        [Test]
-        public async Task ShouldReturnAllFourDirectionsForMovementThroughMaze()
-        {
-          var result = await this.client.GetAsync("/Maze");
-          result.EnsureSuccessStatusCode();
-          var content = JsonConvert.DeserializeObject<string[]>(await result.Content.ReadAsStringAsync());
-          content.Should().Contain("Up");
-          content.Should().Contain("Down");
-          content.Should().Contain("Left");
-          content.Should().Contain("Right");
-        }
+      var factory = new APIWebApplicationFactory();
+      client = factory.CreateClient();
     }
+
+    [Test]
+    public async Task ShouldReturnAllFourDirectionsForMovementThroughMaze()
+    {
+      var result = await client.GetAsync("/Maze");
+      result.EnsureSuccessStatusCode();
+      var content = JsonConvert.DeserializeObject<string[]>(await result.Content.ReadAsStringAsync());
+      content.Should().Contain("Up");
+      content.Should().Contain("Down");
+      content.Should().Contain("Left");
+      content.Should().Contain("Right");
+    }
+
+    [Test]
+    public async Task ShouldReturnTrueForMazeAdd()
+    {
+      var json = new
+      {
+        mazeName = "Test Maze",
+        mazeGrid = @"SOXXXXXXXX
+OOOXXXXXXX
+OXOOOXOOOO
+XXXXOXOXXO
+OOOOOOOXXO
+OXXOXXXXXO
+OOOOXXXXXE"
+      };
+
+      var result = await client.PostAsync("/addmaze", new StringContent(JObject.FromObject(json).ToString()));
+      Assert.IsTrue(result.IsSuccessStatusCode);
+    }
+  }
 }
--- ValantDemoApi/ValantDemoApi/Controllers/MazeController.cs
+++ ValantDemoApi/ValantDemoApi/Controllers/MazeController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace ValantDemoApi.Controllers
 {
@@ -23,5 +27,38 @@ namespace ValantDemoApi.Controllers
       return new List<string> { "Up", "Down", "Left", "Right" };
     }
 
+    [HttpGet("/getmoves")]
+    public IEnumerable<string> GetAvailableMoves(string id, int x, int y)
+    {
+      return _manager.GetNextAvailableMoves(id, x, y);
+    }
+
+    [HttpPost("/addmaze")]
+    public async Task<StatusCodeResult> AddNewMaze()
+    {
+      using (var reader = new StreamReader(Request.Body))
+      {
+        var json = await reader.ReadToEndAsync();
+        var jsonObject = JObject.Parse(json);
+
+        var mazeId = "";
+        if (jsonObject.ContainsKey("mazeId"))
+          mazeId = jsonObject.Value<string>("mazeId");
+        var mazeName = "";
+        if (jsonObject.ContainsKey("mazeName"))
+          mazeName = jsonObject.Value<string>("mazeName");
+        var mazeGrid = jsonObject.Value<string>("mazeGrid");
+
+        _logger.LogDebug($"Received request to add maze: {mazeGrid} ({mazeName} {mazeId})");
+
+        var rows = mazeGrid.Split(new[] { ",", "\n", "\n\r" }, System.StringSplitOptions.RemoveEmptyEntries);
+        var success = await _manager.AddNewMaze(new List<string>(rows), mazeName, mazeId);
+
+        if (success)
+          return StatusCode((int)HttpStatusCode.OK);
+        else
+          return StatusCode((int)HttpStatusCode.BadRequest);
+      }
+    }
   }
 }
